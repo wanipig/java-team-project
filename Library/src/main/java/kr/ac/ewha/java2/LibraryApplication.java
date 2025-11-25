e02519d0831166731d96af415d1333424f9054c8
수안 <rla.swan@ewhain.net>
2025-11-25 17:51:24 +0900

    Library Spring Boot project 생성

---
+++ Library/src/main/java/kr/ac/ewha/java2/LibraryApplication.java
@@ -0,0 +1,14 @@
+package kr.ac.ewha.java2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+		System.out.println("Library Application Start!");
+	}
+
+}
