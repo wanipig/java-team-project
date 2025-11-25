3cc2ae5eb91ec78814aefededdf05d9da2600b2d
수안 <rla.swan@ewhain.net>
2025-11-26 00:25:50 +0900

    configuration, common exception 추가

---
+++ Library/src/main/java/kr/ac/ewha/java2/common/config/WebConfig.java
@@ -0,0 +1,30 @@
+package kr.ac.ewha.java2.common.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class WebConfig {
+	
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+	
+	@Bean
+	public DataSource dataSource() throws Exception{
+		System.out.println("시작!");
+		DataSource dataSource = new HikariDataSource(hikariConfig());
+		return dataSource;
+	}
+}
