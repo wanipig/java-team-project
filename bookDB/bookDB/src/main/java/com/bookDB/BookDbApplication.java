b67fb6ce184dfd7f638bb6aa119bc84c8eddf6b9
dev-ldy03 <dylee0329@gmail.com>
2025-11-26 12:38:37 +0900

    이다영 delete구현 코드 업데이트 완료 커밋

---
+++ bookDB/bookDB/src/main/java/com/bookDB/BookDbApplication.java
@@ -0,0 +1,13 @@
+package com.bookDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDbApplication.class, args);
+	}
+
+}
---
+++ bookDB/bookDB/src/test/java/com/bookDB/BookDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
