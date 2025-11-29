a89da9451ee4311bf41aaef2524f81cc5fadb6e4
수안 <rla.swan@ewhain.net>
2025-11-29 19:17:34 +0900

    new project

---
+++ BooksDB/src/main/java/kr/ac/ewha/java2/BooksDbApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.ewha.java2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksDbApplication.class, args);
+	}
+
+}
